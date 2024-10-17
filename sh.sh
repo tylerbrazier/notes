cd0441653763d4fef22f648887eb23d53cdc4fbd
Tyler Brazier <tyler@tylerbrazier.com>
2024-10-16 21:58:19 -0600

    sh.sh: add parameter expansion

--- sh.sh
+++ sh.sh
@@ -14,3 +14,20 @@ fn() {
 	# the top (the `local` keyword is not part of POSIX, but not needed if
 	# you follow this pattern).
 }
+
+parameter_expansion() {
+	var="one/two/three"
+
+	echo "Parameter Expansion:"
+	echo "# removes pattern from BEGINNING of string"
+	echo "% removes pattern from END of string"
+
+	printf '\nvar=%s\n\n' "$var"
+
+	echo '${var#*/} ->' "${var#*/}"    # two/three
+	echo '${var##*/} ->' "${var##*/}"  # three
+
+	echo '${var%/*} ->' "${var%/*}"    # one/two
+	echo '${var%%/*} ->' "${var%%/*}"  # one
+}
+parameter_expansion
